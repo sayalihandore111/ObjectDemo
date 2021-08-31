504b183a06006d8374b9d448597de76d3a58142f
sayalihandore111 <sayalihandore111@gmail.com>
2021-08-31 23:24:06 +0530

    Add project files.

---
+++ ObjectDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ObjectsDemo
+{
+    class Car
+    {
+        public int Model;
+        public Car(int a)
+        {
+            Model = a;
+        }
+        public void Display_Model()
+        {
+            Console.WriteLine(Model);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car(22);
+            Ford.Model = 10;
+            Car BMW = Ford;
+            BMW.Display_Model();
+            Ford.Display_Model();
+        }
+    }
+}
